6be08834cfd170616f9babfdbc6344008a7df874
Kushiie <riviere.lisa1875@gmail.com>
2022-02-27 13:19:54 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 // Permet d'importer les différents modules
 import session from "express-session";
+import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+
+// Importer les routes
+import dashRoute from "./routes/dashboard";
+import authRoute from "./routes/auth";
+
+// Importation de redirectAuth
+import { checkAuth } from "./utils"
  
 // Permet d'ajouter les éléments du fichier .env à process
 dotenv.config();
@@ -21,11 +29,20 @@ app.use(express.json());
 // Middleware session
 app.use(session({ secret: 'secretSession', resave: true, saveUninitialized: true }));
 
-// Routeur personnalisé => Accueil
-app.use("/", require('../src/routes/home'));
+// Page d'Accueil
+app.get('/', checkAuth, (req, res) => res.render('home'));
 
 // Routeur personnalisé => Auth
-app.use("/auth", require('../src/routes/auth'));
+app.use('/auth', authRoute);
+
+// Routeur personnalisé => Auth
+app.use('/dashboard', dashRoute);
+
+// Page d'erreur
+app.get('*', (req, res) => res.status(404).render('error/404'));
+
+// Connection à mongoDB
+mongoose.connect(process.env.mongoDBConnection!).then(() => console.log("[System] Database connected !"));
 
 // Écoute sur un port (3000 par défault)
 app.listen(process.env.port || "3000", () => console.log(`[System] App listen on port ${process.env.port || "3000"}`));
